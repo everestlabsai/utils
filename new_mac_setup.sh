14f6391465ab8608f2c4c4341ed0303618675239
Pratik Khadloya <tispratik@gmail.com>
2019-02-09 00:49:10 -0800

    New mac setup automation script

---
+++ new_mac_setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Chrome
+brew cask install google-chrome
+
+# Intellij
+brew cask install intellij-idea-ce
+
+# Iterm
+brew cask install iterm2
+
+# Flock
+brew cask install flock
+
+# Flycut
+brew cask install flycut
+
+# Atom Editor
+brew cask install atom
+
+# MacVim
+brew cask install macvim
+
+# Dropbox
+brew cask install dropbox
